dd5cec4e262dc04a3580a33e261af76dd3febcf2
rachynska-anastasiia <rachynska.anastasiia.work@gmail.com>
2026-04-17 13:01:37 +0300

    готова лаба 4

--- lab_4_client.cpp
+++ lab_4_client.cpp
@@ -4,7 +4,7 @@
 #include <iomanip>
 #include <vector>
 
-#define PORT 666
+#define PORT 888
 #define SERVERADDR "127.0.0.1"
 
 using namespace std;
@@ -12,24 +12,150 @@ using namespace std;
 void printMatrix(vector<int> matrix, int total_size_matrix) {
     for (int i = 0; i < total_size_matrix; i++) {
         for (int j = 0; j < total_size_matrix; j++) {
-            cout << setw(4) << matrix[i*total_size_matrix+j] << " ";
+            cout << setw(4) << matrix[i * total_size_matrix + j] << " ";
         }
         cout << endl;
     }
 }
 
-void generateMatrix(vector<int>& matrix, int N) {
+void generateMatrix(vector<int> &matrix, int N) {
     srand(time(NULL));
     for (int i = 0; i < N; i++)
-        matrix[i] = rand()%100 +1;
+        matrix[i] = rand() % 100 + 1;
 }
 
-int main() {
-    printf("TCP CLIENT\n");
+void client_process(SOCKET my_socket) {
+    char tag, response;
+    vector<int> result_matrix;
+    vector<int> matrix;
     vector<char> packet(5);
+    int N, threads_num, total_size_matrix, temp, packet_size, input_tag;
+
+    while (true) {
+        cout << "Enter tag = ";
+        cin >> input_tag;
+        tag = char(input_tag);
+
+        //надсилаємо рзмір матриці
+        if (tag == 0x01) {
+            N = 0;
+            while (N <= 0) {
+                cout << "Enter number of rows and columns: ";
+                cin >> N;
+            }
+            total_size_matrix = N * N;
+            temp = htonl(N);
+            packet[0] = 0x01;
+            memcpy(&packet[1], &temp, sizeof(int));
+            if (send(my_socket, packet.data(), 5, 0) == SOCKET_ERROR) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+
+            //створюємо матриці
+            matrix.resize(total_size_matrix, 0);
+            result_matrix.resize(total_size_matrix, 0);
+            generateMatrix(matrix, total_size_matrix);
+            printMatrix(matrix, N);
+
+            //змінюємо розмір пакета
+            packet_size = total_size_matrix * sizeof(int) + 5;
+            if (packet.size() < packet_size) packet.resize(packet_size);
+
+        //надсилаємо кількість потоків
+        } else if (tag == 0x02) {
+            threads_num = 0;
+            while (threads_num <= 0) {
+                cout << "Enter number of threads: ";
+                cin >> threads_num;
+            }
+            temp = htonl(threads_num);
+            packet[0] = 0x02;
+            memcpy(&packet[1], &temp, sizeof(int));
+            if (send(my_socket, packet.data(), 5, 0) == SOCKET_ERROR) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+
+        //надсилаємо дані
+        } else if (tag == 0x03) {
+            packet[0] = 0x03;
+            int counter = htonl(total_size_matrix);
+            memcpy(&packet[1], &counter, sizeof(int));
+            for (size_t i = 0; i < total_size_matrix; i++) {
+                temp = htonl(matrix[i]);
+                memcpy(&packet[5 + i * sizeof(int)], &temp, sizeof(int));
+            }
+            if (send(my_socket, packet.data(), total_size_matrix * sizeof(int) + 5, 0) == SOCKET_ERROR) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+            if (recv(my_socket, &response, 1, 0) <= 0) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+            if (response == 0x03) cout << "data was sent" << endl;
+            else cout << "matrix is empty" << endl;
+
+        //просимо обробити
+        } else if (tag == 0x04) {
+            packet[0] = 0x04;
+            if (send(my_socket, packet.data(), 1, 0) == SOCKET_ERROR) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+            if (recv(my_socket, &response, 1, 0) <= 0) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+            if (response == 0x09) cout << "uninitialized" << endl;
+            else if (response == 0x04) {
+                if (recv(my_socket, &response, 1, 0)<= 0) {
+                    cout << "server doesn't work" << endl;
+                    return;
+                }
+                cout << "request to start was sent" << endl;
+            }
+
+        //отримуємо результати
+        } else if (tag == 0x05) {
+            packet[0] = 0x05;
+            if (send(my_socket, packet.data(), 1, 0) == SOCKET_ERROR) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+            if (recv(my_socket, &response, 1, 0)<=0) {
+                cout << "server doesn't work" << endl;
+                return;
+            }
+            if (response == 0x05) {
+                for (int i = 0; i < total_size_matrix; i++) {
+                    if (recv(my_socket, (char *) &temp, 4, 0)<= 0) {
+                        cout << "server doesn't work" << endl;
+                        return;
+                    }
+                    result_matrix[i] = ntohl(temp);
+                }
+                printMatrix(result_matrix, N);
+            } else if (response == 0x07) cout << "not started" << endl;
+            else if (response == 0x08) cout << "in progress" << endl;
+            else if (response == 0x09) cout << "uninitialized" << endl;
+
+        //відключаємося
+        } else if (tag == 0x06) {
+            packet[0] = 0x06;
+            send(my_socket, packet.data(), 1, 0);
+            break;
+        }
+    }
+}
+
+
+int main() {
+    cout << "TCP CLIENT\n";
 
     WSADATA wsa;
-    WSAStartup(MAKEWORD(2,2), &wsa);
+    WSAStartup(MAKEWORD(2, 2), &wsa);
 
     SOCKET my_socket;
     if ((my_socket = socket(AF_INET,SOCK_STREAM, 0)) == INVALID_SOCKET) {
@@ -46,79 +172,22 @@ int main() {
         return -1;
     }
 
-    if (connect(my_socket, (struct sockaddr *) &dest_addr, sizeof(dest_addr))) {
+    if (connect(my_socket, (sockaddr *) &dest_addr, sizeof(dest_addr))) {
         cout << "Connect error " << WSAGetLastError();
         return -1;
     }
 
-    int N, threads_num, total_size_matrix, temp, result;
-    cout << "Enter number of rows and columns: ";
-    cin >> N;
-    total_size_matrix = N*N;
-    temp = htonl(N);
-    packet[0]=0x01;
-    memcpy(&packet[1], &temp, sizeof(int));
-    result = send(my_socket, packet.data(), 5, 0);
+    cout << "TAGS:" << endl;
+    cout << "1 - enter number of rows and columns" << endl;
+    cout << "2 - enter number of threads" << endl;
+    cout << "3 - send matrix" << endl;
+    cout << "4 - start transposing" << endl;
+    cout << "5 - receive status / results" << endl;
+    cout << "6 - exit" << endl << endl;
 
-    cout << "Enter number of threads: ";
-    cin >> threads_num;
-    temp = htonl(threads_num);
-    packet[0]=0x02;
-    memcpy(&packet[1], &temp, sizeof(int));
-    result = send(my_socket, packet.data(), 5, 0);
-
-    cout << total_size_matrix << endl;
-    vector matrix(total_size_matrix, 0);
-    vector result_matrix(total_size_matrix, 0);
-    generateMatrix(matrix, total_size_matrix);
-    printMatrix(matrix, N);
-
-    int packet_size = total_size_matrix*sizeof(int)+5;
-    if (packet.size() < packet_size) {
-        packet.resize(packet_size);
-    }
-
-    packet[0]=0x03;
-    int counter = htonl(total_size_matrix);
-    memcpy(&packet[1], &counter, sizeof(int));
-    for (size_t i = 0; i < total_size_matrix; i++) {
-        temp = htonl(matrix[i]);
-        memcpy(&packet[5+i*sizeof(int)], &temp, sizeof(int));
-    }
-    send(my_socket, packet.data(), packet_size, 0);
-
-    //packet.resize(1);
-    packet[0]=0x04;
-    send(my_socket, packet.data(), 1, 0);
-    char response;
-    recv(my_socket, &response, 1, 0);
-    cout << response << endl;
-
-    packet[0]=0x05;
-    send(my_socket, packet.data(), 1, 0);
-    recv(my_socket, &response, 1, 0);
-
-    if (response == 0x05) {
-        for (int i = 0; i < total_size_matrix; i++) {
-            recv(my_socket, (char*)&temp, 4, 0);
-            result_matrix[i]= ntohl(temp);
-        }
-        printMatrix(result_matrix, N);
-    }
-
-    packet[0]=0x06;
-    send(my_socket, packet.data(), 1, 0);
+    client_process(my_socket);
 
     closesocket(my_socket);
     WSACleanup();
     return 0;
 }
-
-/*надсилаємо значення серверу та отримуємо відповідь про успішність відправлення
-    LARGE_INTEGER frequency, start_time, end_time;
-QueryPerformanceFrequency(&frequency);
-QueryPerformanceCounter(&start_time);
-
-QueryPerformanceCounter(&end_time);
-double duration = (double) (end_time.QuadPart - start_time.QuadPart) / frequency.QuadPart;
-*/
--- lab_4_server.cpp
+++ lab_4_server.cpp
@@ -4,22 +4,22 @@
 #include <iomanip>
 #include <thread>
 #include <vector>
-#define MY_PORT 666
+#include <conio.h>
+#define MY_PORT 888
 
 using namespace std;
 
-void task(vector<int>& result_matrix, vector<int>& matrix, int N, int start, int end) {
+void task(vector<int> &result_matrix, vector<int> &matrix, int N, int start, int end) {
     for (int i = start; i < end; i++) {
-        for (int j = 0; j < N; j++) {
-            result_matrix[j*N+i] = matrix[i*N+j];
-        }
+        for (int j = 0; j < N; j++)
+            result_matrix[j * N + i] = matrix[i * N + j];
     }
 }
 
 void printMatrix(vector<int> matrix, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cout << setw(4) << matrix[i*N+j] << " ";
+            cout << setw(4) << matrix[i * N + j] << " ";
         }
         cout << endl;
     }
@@ -28,152 +28,139 @@ void printMatrix(vector<int> matrix, int N) {
 void work_with_client(SOCKET socket) {
     vector<int> result_matrix;
     vector<int> matrix;
-    char tag;
+    char tag, error_tag = 0x09, not_started_teg = 0x07, in_process = 0x08;
     int N, temp, threads_number, counter, size, i;
+    bool has_size = false, has_threads = false, has_numbers = false, processing = false, done = false;
+    unsigned long mode = 0;
+    ioctlsocket(socket, FIONBIO, &mode);
 
-    while (recv(socket, &tag, 1, 0)!=0x06) {
+    while (recv(socket, &tag, 1, 0) > 0) {
+        //читаємо розмірність матриці
         if (tag == 0x01) {
-            recv(socket, (char*)&temp, 4, 0);
-            N=ntohl(temp);
+            recv(socket, (char *) &temp, 4, 0);
+            N = ntohl(temp);
             cout << "N = " << N << endl;
+            matrix.resize(N * N);
+            result_matrix.resize(N * N);
+            has_size = true;
         }
-        else if (tag == 0x02) {
-            recv(socket, (char*)&temp, 4, 0);
-            threads_number=ntohl(temp);
-            cout << "theads_number = " << threads_number << endl;
-        }
-        else if (tag == 0x03) {
-            recv(socket, (char*)&counter, 4, 0);
-            int counter=ntohl(counter);
-            matrix.resize(counter);
 
-            for (i = 0; i < counter; i++) {
-                recv(socket, (char*)&temp, 4, 0);
-                matrix[i] = ntohl(temp);
-            }
-            printMatrix(matrix, N);
+        //читаємо кількість потоків для виконання задачі
+        else if (tag == 0x02) {
+            recv(socket, (char *) &temp, 4, 0);
+            threads_number = ntohl(temp);
+            cout << "theads_number = " << threads_number << endl;
+            has_threads = true;
         }
+
+        //отримуємо матрицю
+        else if (tag == 0x03) {
+            recv(socket, (char *) &counter, 4, 0);
+            counter = ntohl(counter);
+            if (counter != N * N || counter == 0) {
+                vector<char> junk(counter * 4);
+                if (counter > 0) recv(socket, junk.data(), counter * 4, 0);
+                send(socket, (char *) &error_tag, 1, 0);
+            } else {
+                for (i = 0; i < counter; i++) {
+                    recv(socket, (char *) &temp, 4, 0);
+                    matrix[i] = ntohl(temp);
+                }
+                has_numbers = true;
+                printMatrix(matrix, N);
+                send(socket, (char *) &tag, 1, 0);
+            }
+        }
+
+        //запускаємо обчислення
         else if (tag == 0x04) {
-            result_matrix.resize(N*N);
-            vector<thread> threads;
-            size=N/threads_number;
-            for (i = 0; i < threads_number-1; i++) {
-                threads.emplace_back(task, ref(result_matrix), ref(matrix), N, i*size, (i+1)*size);
+            if (!has_numbers || !has_size || !has_threads)
+                send(socket, &error_tag, 1, 0);
+            else {
+                send(socket, &tag, 1, 0);
+
+                vector<thread> threads;
+                size = N / threads_number;
+
+                processing = true;
+                for (i = 0; i < threads_number - 1; i++)
+                    threads.emplace_back(task, ref(result_matrix), ref(matrix), N, i * size, (i + 1) * size);
+                threads.emplace_back(task, ref(result_matrix), ref(matrix), N, i * size, N);
+
+                for (i = 0; i < threads_number; i++) threads[i].join();
+                done = true;
+                processing = false;
+                send(socket, &tag, 1, 0);
             }
-            threads.emplace_back(task, ref(result_matrix), ref(matrix), N, i*size, N);
-            for (i = 0; i < threads_number; i++) {
-                threads[i].join();
-            }
-            send(socket, &tag, 1, 0);
         }
+
+        //надсилаємо результат
         else if (tag == 0x05) {
-            send(socket, &tag, 1, 0);
-            for (i = 0; i < N*N; i++) {
-                temp = htonl(result_matrix[i]);
-                send(socket, (char*)&temp, 4, 0);
-            }
+            if (!has_numbers || !has_size || !has_threads)
+                send(socket, &error_tag, 1, 0);
+            else if (done) {
+                send(socket, &tag, 1, 0);
+                for (i = 0; i < N * N; i++) {
+                    temp = htonl(result_matrix[i]);
+                    send(socket, (char *) &temp, 4, 0);
+                }
+            } else if (processing) send(socket, &in_process, 1, 0);
+            else send(socket, &not_started_teg, 1, 0);
+        } else if (tag == 0x06) {
+            closesocket(socket);
+            return;
         }
-        else if (tag == 0x06)closesocket(socket);
     }
 }
 
 int main() {
-    printf("TCP SERVER \n");
+    cout << "TCP SERVER \n";
 
-    //ініціалізація бібліотеки
     WSADATA wsa;
-    WSAStartup(0x0202, &wsa);
-
-    //створення сокера для сервера
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+   
     SOCKET my_socket;
     if ((my_socket = socket(AF_INET,SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Error socket", WSAGetLastError());
+        cout << "Error socket " << WSAGetLastError();
         WSACleanup();
         return -1;
     }
 
-    // пов'язування сокета з локальним адресом
     sockaddr_in local_addr;
     local_addr.sin_family = AF_INET;
     local_addr.sin_port = htons(MY_PORT);
     local_addr.sin_addr.s_addr = 0; //слухаємо всі ір //INADDR_ANY
 
-    //викликаємо bind для зв'язування
     if (bind(my_socket, (sockaddr *) &local_addr, sizeof(local_addr))) {
-        printf("Error bind %d\n", WSAGetLastError());
+        cout << "Error bind %d\n" << WSAGetLastError();
         closesocket(my_socket);
         WSACleanup();
         return -1;
     }
 
-    // очікування підключення
-    printf("Waiting for connecting...\n");
+    cout << "Waiting for connecting..." << endl;
+    cout << "Enter q to exit" << endl;
+
     if (listen(my_socket, 0x100)) {
-        printf("Error listen %d\n", WSAGetLastError());
+        cout << "Error listen %d\n" << WSAGetLastError();
         closesocket(my_socket);
         WSACleanup();
         return -1;
     }
 
-    // клієнтський сокет
+    unsigned long mode = 1;
+    ioctlsocket(my_socket, FIONBIO, &mode);
     while (true) {
-        SOCKET client_socket= accept(my_socket, NULL, NULL);
-        if (client_socket != INVALID_SOCKET) {
+        if (_kbhit())
+            if (_getch() == 'q') break;
+
+        SOCKET client_socket = accept(my_socket, NULL, NULL);
+        if (client_socket != INVALID_SOCKET)
             thread(work_with_client, client_socket).detach();
-        }
+        else Sleep(10);
     }
 
     closesocket(my_socket);
     WSACleanup();
-
     return 0;
 }
-
-/*
-//початок замірів часу для отримання чисел (а також їх розмірів)
-    LARGE_INTEGER frequency, start_time, end_time;
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&start_time);
-
-
-    QueryPerformanceCounter(&end_time);
-    double duration = (double) (end_time.QuadPart - start_time.QuadPart) / frequency.QuadPart;
-
-    //підтверджуємо отримання даних
-    send(client_socket, "Data received", sizeof("Data received"), 0);
-
-    //друк отриманих значень
-    for (int i = 0; i < 2; i++) {
-        printf("double_numbers[%d] = %f\n", i, double_numbers[i]);
-    }
-    for (int i = 0; i < 9; i++) {
-        printf("float_numbers[%d] = %f\n", i, float_numbers[i]);
-    }
-
-    //Друк результатів по обрахованому часу й розміру
-    printf("Elapsed time: %f\n", duration);
-    printf("Total number of bytes received: %d\n", num_of_bytes_received);
-    if (num_of_bytes_received == num_of_bytes_expected) {
-        printf("All the data was received");
-    } else {
-        printf("Something was missed...");
-    }
- */
-
-/*
-    sockaddr_in client_addr;
-    int client_addr_size = sizeof(client_addr);
-    SOCKET client_socket = accept(my_socket, (sockaddr *) &client_addr, &client_addr_size);
-
-    //отримуємо очікуваний розмір даних
-    temp = recv(client_socket, (char *) &num_of_bytes_expected, sizeof(int), 0);
-    if (temp <= 0) {
-        printf("Failed to get expected size\n");
-        closesocket(client_socket);
-        closesocket(my_socket);
-        WSACleanup();
-        return -1;
-    }
-    printf("Expected to receive: %d bytes\n", num_of_bytes_expected);
-
-   */
